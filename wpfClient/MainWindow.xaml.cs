f87944b4515e2a9d3e3a8ebaa03ff5328707e505
futte <ntthyssen@gmail.com>
2019-10-22 13:56:08 +0200

    init projekt commit

---
+++ wpfClient/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace wpfClient
+{
+    /// <summary> 
+    /// SignalR client hosted in a WPF application. The client 
+    /// lets the user pick a user name, connect to the server asynchronously 
+    /// to not block the UI thread, and send chat messages to all connected  
+    /// clients whether they are hosted in WinForms, WPF, or a web application. 
+    /// For simplicity, MVVM will not be used for this sample. 
+    /// </summary> 
+    public partial class MainWindow : Window
+    {
+        /// <summary> 
+        /// This name is simply added to sent messages to identify the user; this  
+        /// sample does not include authentication. 
+        /// </summary> 
+        
+        NetComm.Client client; //The client object used for the communication
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            client = new NetComm.Client(); //Initialize the client object
+
+            //Adding event handling methods for the client
+            client.Connected += new NetComm.Client.ConnectedEventHandler(client_Connected);
+            client.Disconnected += new NetComm.Client.DisconnectedEventHandler(client_Disconnected);
+            client.DataReceived += new NetComm.Client.DataReceivedEventHandler(client_DataReceived);
+
+            //Connecting to the host
+            client.Connect("10.16.184.154", 3000, "Jack"); //Connecting to the host (on the same machine) with port 2020 and ID "Jack"
+
+            Button btn = new Button();
+            btn.Name = "Send";
+            btn.Click += Send_Click;
+            //he
+            String hey = "heheye";
+        }
+
+
+        void client_DataReceived(byte[] Data, string ID)
+        {
+            Chat.AppendText(ID + ": " + ConvertBytesToString(Data) + Environment.NewLine); //Updates the log with the current connection state
+        }
+      
+
+        void client_Disconnected()
+        {
+            Chat.AppendText("Disconnected from host!" + Environment.NewLine); //Updates the log with the current connection state
+        }
+
+        void client_Connected()
+        {
+            Chat.AppendText("Connected succesfully!" + Environment.NewLine); //Updates the log with the current connection state
+        }
+
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            client.SendData(ConvertStringToBytes(MessageTextBox.Text)); //Sends the message to the host
+            Chat.AppendText("\n " + MessageTextBox.Text);
+            MessageTextBox.Clear(); //Clears the chatmessage textbox text
+            
+        }
+
+        string ConvertBytesToString(byte[] bytes)
+        {
+            return ASCIIEncoding.ASCII.GetString(bytes);
+        }
+
+        byte[] ConvertStringToBytes(string str)
+        {
+            return ASCIIEncoding.ASCII.GetBytes(str);
+        }
+
+     }
+}
+
+
